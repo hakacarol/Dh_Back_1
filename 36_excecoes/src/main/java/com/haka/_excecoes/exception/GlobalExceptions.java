d8dcb41af5dbf287c4ec2a3cc3eb3dbf7a82a639
hakacarol <haka.carolina@gmail.com>
2021-12-02 11:41:09 -0300

    subindo aulas

--- 36_excecoes/src/main/java/com/haka/_excecoes/exception/GlobalExceptions.java
+++ 36_excecoes/src/main/java/com/haka/_excecoes/exception/GlobalExceptions.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
-public class GlobalExceptions extends Exception{
+public class GlobalExceptions{
 
     @ExceptionHandler({BadRequestException.class})
     public ResponseEntity<String> processarBadRequest (BadRequestException exception) {
